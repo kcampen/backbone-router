51240bd8b398d96808b32f095046699336b43f78
Katy Campen <katycampen@gmail.com>
2014-02-25 15:59:31 -0500

    working routers confirm

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,8 @@ console.log('p i z z a');
 
 $(document).ready(function(){
 
+	toppings = new ZaCollection(data)
+
 	router = new MainRouter();
 
 	Backbone.history.start();
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,32 +3,32 @@ var MainRouter = Backbone.Router.extend({
 	routes: {
 		"pizza" : "main",
 		"pizza/:name" : "allToppings",
-		"pizza/allToppings/:name" : "top"
 	},
 
 	initialize: function(){
 		console.log('hey guys');
 	},
 
-	main: function(name){
+	main: function(){
 		new PizzaView()
 
 	},
 
 	allToppings: function(topping){
-		var toppings = new ZaCollection(data)
 
-		toppings.each(function(za){
-			new ToppingView({model: za});
-		})
-	},
-	 top: function(){
-		var toppings = new ZaCollection(data)
- 		toppings_.findWhere(data, {topping:'cheese'});
-			new ToppingView({model: za});
- 	}
+		$('.jumbotron').html('');
+		
+		var toppingsToShow = toppings.findWhere({topping: topping});
 
+			console.log('hey guys', topping)
+			new ToppingView({model: toppingsToShow});
+	}
 
+	 // top: function(){
+		// var toppings = new ZaCollection(data)
+ 	// 	toppings.findWhere(data, {topping:"cheese"});
+		// 	new ToppingView({model: za});
+ 	// }
 
 
 
